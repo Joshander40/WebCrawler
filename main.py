1aaf189770165a880241d93600feeb000dc1de6b
SpicyChipGuy <jixaxxx@gmail.com>
2022-12-05 14:36:08 -0600

    Before new spider is made for user choice.

--- main.py
+++ main.py
@@ -100,20 +100,23 @@ while True:
         # Initial first 10 database display
         break;
     if event =="-TABLE-":
-        print(values[event][0])
+        print(values[event][0])                                       # Printing index of column 1 link
         # Pass in a new URL for crawling and overwrite the file
-        index = values[event][0]
-        print(table_array[index][0])
+        index = values[event][0]                                      # setting index from values table 
+        print(table_array[index][0])                                  # Printing link from specified index of column 1
         # new name. Probably the url
-        # NAME = values.read()         
+        NAME = index                                                  # values.read()  - not totally sure if an int is ok here? 
+        # NAME = values.read()                                        # AttributeError: 'dict' object has no attribute 'read'
+        print(NAME)
         # New url here
-        # HOME_PAGE = "https://www.sbnation.com/college-football/"    # this will be the user's selected URL
-        # DOMAIN_NAME = getDomainName(HOME_PAGE)
-        # # I think we should overwrite this file
+        HOME_PAGE = table_array[index][0];              # this will be the user's selected URL (EX: https://www.sbnation.com/college-football/)
+        DOMAIN_NAME = getDomainName(HOME_PAGE)          # Get domain name from selected URL
+        # print(DOMAIN_NAME)
+        # I think we should overwrite this file
         # QUEUE_FILE = NAME + "/queue.txt"
         # # This too? Not sure how this works
         # CRAWLED_FILE = NAME + "/crawled.txt"
-        # NUM_OF_THREADS = 8
+        # # NUM_OF_THREADS = 8
         # queue = Queue()
         # Spider(NAME, HOME_PAGE, DOMAIN_NAME)
 
@@ -127,7 +130,7 @@ while True:
         
 
         # Update database function
-        break;
+        #break;
 
 window.close()
 
